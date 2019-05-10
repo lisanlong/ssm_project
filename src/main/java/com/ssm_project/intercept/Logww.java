662af14a94992df80c8fbbad6480dd35ec6d0570
lisanlong <1119753268@qq.com>
2019-05-10 11:21:14 +0800

    原始ssm_project

--- src/main/java/com/ssm_project/intercept/Logww.java
+++ src/main/java/com/ssm_project/intercept/Logww.java
@@ -1,20 +1,9 @@
 package com.ssm_project.intercept;
-
-
-
 import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @Documented
 public @interface Logww {
-
-
-
-
-
-
-
-
       String operationType() default "";
 
 
